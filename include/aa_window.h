c3cc3b5e9f5506ae47671e7731e693ebc9f5930c
hw <huawei.william@bytedance.com>
2019-12-26 23:09:00 +0800

    refactor: move implementation of windowSum from .h to .cpp

--- include/aa_window.h
+++ include/aa_window.h
@@ -10,23 +10,17 @@ namespace libaa
 class Window
 {
 public:
+    /**
+     * Returns a window of a given length and type.
+     */
     static Eigen::ArrayXf getWindow(WindowType win_type, size_t win_size);
 
-    static Eigen::ArrayXf windowSum(WindowType win_type, size_t n_frames, size_t win_size, size_t hop_size)
-    {
-        size_t n = (n_frames - 1) * hop_size + win_size;
-        Eigen::ArrayXf x(n);
-
-        Eigen::ArrayXf window = getWindow(win_type, win_size).array();
-
-        for(int i = 0;i < n_frames; ++i)
-        {
-            const int overlap_add_i = i * hop_size;
-            x.segment(overlap_add_i, win_size) += window;
-        }
-
-        return x;
-    }
+    /**
+     * Returns a overlap window sum.
+     *
+     * ISTFT uses overlap window sum to recover signal.
+     */
+    static Eigen::ArrayXf windowSum(WindowType win_type, size_t n_frames, size_t win_size, size_t hop_size);
 };
 
 }
--- src/dsp/aa_window.cpp
+++ src/dsp/aa_window.cpp
@@ -36,6 +36,22 @@ float hanningAtPoint(float point, int numSamples){
     return 0.5f * ( 1.0f - cosf ( (2.0f * M_PI * point) / ( (float)numSamples - 1.0f) ) );
 }
 
+Eigen::ArrayXf Window::windowSum(WindowType win_type, size_t n_frames, size_t win_size, size_t hop_size)
+{
+    size_t n = (n_frames - 1) * hop_size + win_size;
+    Eigen::ArrayXf x(n);
+
+    Eigen::ArrayXf window = getWindow(win_type, win_size).array();
+
+    for(int i = 0;i < n_frames; ++i)
+    {
+        const int overlap_add_i = i * hop_size;
+        x.segment(overlap_add_i, win_size) += window;
+    }
+
+    return x;
+}
+
 Eigen::ArrayXf Window::getWindow(WindowType win_type, size_t win_size)
 {
     std::function<float(int,int)> win_func = hammingAtPoint;
